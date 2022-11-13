fdabef2a5aa997f4e3992d681a5bada0446b98b2
Raquel <rauqel@pollex.com.tw>
2022-11-13 14:29:16 +0800

    sinature 加入 navbar

--- src/app/pdf-signature/pdf-signature.component.ts
+++ src/app/pdf-signature/pdf-signature.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pdf-signature.component.scss']
 })
 export class PdfSignatureComponent implements OnInit {
-
+  links = [
+    { name: '邀請他人簽署', navigate: '', disable: true },
+    { name: '簽署新文件', navigate: 'signature-new-document', disable: false },
+    { name: '登入', navigate: 'login', disable: false },
+  ]
   constructor() { }
 
   ngOnInit(): void {
